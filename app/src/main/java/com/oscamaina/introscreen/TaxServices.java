08785e22d4099453e221afea76c173c79d2c20ed
oscamaina <mainaoscar40@gmail.com>
2019-12-17 17:01:59 +0300

    Login JSON parsing

---
+++ app/src/main/java/com/oscamaina/introscreen/TaxServices.java
@@ -0,0 +1,14 @@
+package com.oscamaina.introscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaxServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tax_services);
+    }
+}
